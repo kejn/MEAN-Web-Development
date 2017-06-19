fec2b0eac2ac252664a3c5dc995e22653f7d9091
kejn <kaen04@gmail.com>
2017-06-19 17:29:11 +0200

    Configuring an Express application

---
+++ 3. Building an Express Web Application/config/env/development.js
@@ -0,0 +1,6 @@
+module.exports = {
+	"require": function() {
+		return require('morgan');
+	},
+	"param": "dev"
+};
--- 3. Building an Express Web Application/config/express.js
+++ 3. Building an Express Web Application/config/express.js
@@ -1,7 +1,23 @@
-var express = require('express');
+var express = require('express'),
+	bodyParser = require('body-parser'),
+	methodOverride = require('method-override');
 
 module.exports = function() {
 	var app = express();
+	
+	var config = require('./config.js');
+	if (config.param) {
+		app.use(config.require()(config.param));
+	} else {
+		app.use(config.require()());
+	}
+	
+	app.use(bodyParser.urlencoded({
+		extended: true
+	}));
+	app.use(bodyParser.json());
+	app.use(methodOverride());
+	
 	require('../app/routes/index.server.routes.js')(app);
 	return app;
 };

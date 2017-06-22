6020316696ae7fab2c170a8b5462569ae621142d
kejn <kaen04@gmail.com>
2017-06-22 14:52:26 +0200

    Definig custom models

--- 5. Introducing Mongoose/app/models/user.server.model.js
+++ 5. Introducing Mongoose/app/models/user.server.model.js
@@ -4,9 +4,39 @@ var mongoose = require('mongoose'),
 var UserSchema = new Schema({
 	firstName: String,
 	lastName: String,
-	email: String,
-	username: String,
-	password: String
+	email: {
+		type: String,
+		index: true // WARNING: indexes are automatically created at startup!
+	},
+	username: {
+		type: String,
+		trim: true,
+		unique: true
+	},
+	password: String,
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	website: {
+		type: String,
+		get: function(url) {
+			if (url && url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0) {
+				url = 'http://' + url;
+			}
+			return url;
+		}
+	}
 });
+
+UserSchema.virtual('fullName').get(function() {
+	return this.firstName + ' ' + this.lastName;
+}).set(function(fullName) {
+	var splitName = fullName.split(' ');
+	this.firstName = splitName[0] || '';
+	this.lastName = splitName[1] || '';
+});
+
+UserSchema.set('toJSON', { getters: true });
 
 mongoose.model('User', UserSchema);
